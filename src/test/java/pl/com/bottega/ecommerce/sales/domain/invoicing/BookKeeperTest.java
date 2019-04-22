14306ca4562e11995da709ef04d8d286f700c057
misior.justyna <195018@edu.p.lodz.pl>
2019-04-22 17:25:07 +0200

    isolation Added test requestingInvoiceWithTwoPositionShouldCallCalculateTaxMethodTwice

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
@@ -32,5 +33,25 @@ public class BookKeeperTest {
         assertThat(invoice.getItems().size(), is(EXPECTED_VALUE));
     }
 
+    @Test
+    public void requestingInvoiceWithTwoPositionShouldCallCalculateTaxMethodTwice() {
+        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
 
+        Mockito.when(taxPolicy.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO, "test"));
+
+        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
+        InvoiceRequest invoiceRequest = new InvoiceRequest(new ClientData(new Id("195018"), "Justyna"));
+        ProductData productData1 = Mockito.mock(ProductData.class);
+        invoiceRequest.add(new RequestItem(productData1, 1, Money.ZERO));
+        ProductData productData2 = Mockito.mock(ProductData.class);
+        invoiceRequest.add(new RequestItem(productData2,1, Money.ZERO));
+
+        Mockito.when(productData1.getType()).thenReturn(ProductType.STANDARD);
+        Mockito.when(productData2.getType()).thenReturn(ProductType.STANDARD);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        Mockito.verify(taxPolicy, Mockito.times(2)).calculateTax(ProductType.STANDARD, Money.ZERO);
+
+    }
 }
